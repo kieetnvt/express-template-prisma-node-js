08769e5c4cbbe895196fb44ae132a99f85a7dfb3
kieetnvt <kietnvt.it@gmail.com>
2026-07-10 11:00:35 +0700

    refactor: add request pipeline middleware

--- src/api/routes/validators/user.validator.ts
+++ src/api/routes/validators/user.validator.ts
@@ -1,5 +1,9 @@
 import Joi from 'joi';
 
+export const userIdParamsSchema = Joi.object({
+  id: Joi.string().pattern(/^(me|[1-9]\d*)$/).required(),
+});
+
 export const updateProfileSchema = Joi.object({
   name: Joi.string().min(1).max(100).optional(),
   email: Joi.string().email().optional(),
